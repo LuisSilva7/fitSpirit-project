a38c08c480d3f40a36c694b492613d7d8adef52e
Luis <lmpsilva7@gmail.com>
2024-07-04 02:59:35 +0100

    Made first installs and configs

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { initializeApp } from "firebase/app";
+import "./assets/styles/global.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+};
 
-const app = createApp(App)
+initializeApp(firebaseConfig);
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
---
+++ src/views/frontoffice/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>HOMEEEEEVIEWWWWWW</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
