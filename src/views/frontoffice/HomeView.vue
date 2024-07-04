7d4c8e2cd0851908a1bb4c2998f6ca4e50c2642d
Luis <lmpsilva7@gmail.com>
2024-07-04 03:20:54 +0100

    home test

--- src/views/frontoffice/HomeView.vue
+++ src/views/frontoffice/HomeView.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>HOMEEEEEVIEWWWWWW</h1>
+  <button @click="signInWithGoogle">Login</button>
 </template>
 
-<script>
-export default {};
+<script setup>
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+
+const signInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(getAuth(), provider)
+    .then((result) => {
+      console.log(result.user);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 </script>
 
 <style></style>
